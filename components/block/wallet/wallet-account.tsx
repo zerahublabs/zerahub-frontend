98ed9ce154ed7bc662bd17b7b819ab680871177f
billalxcode <billal.xcode@gmail.com>
2025-05-17 16:55:25 +0700

    fix: restructure WalletAccount component for improved layout and disconnect functionality

--- components/block/wallet/wallet-account.tsx
+++ components/block/wallet/wallet-account.tsx
@@ -41,46 +41,48 @@ export default function WalletAccount() {
                     </div>
                 </Button>
             </DrawerTrigger>
-            <DrawerContent className="rounded-tl-2xl rounded-bl-2xl w-[100px] space-y-2">
-                <DrawerHeader>
-                    <DrawerTitle>Account</DrawerTitle>
-                </DrawerHeader>
-                <div className="flex flex-col gap-4">
-                    <div className="flex items-center justify-between px-4">
-                        <div className="inline-flex items-center gap-4">
-                            <AvatarImage string={address} size={40} />
-                            <div className="flex flex-col justify-center">
-                                <p className="font-semibold text-foreground">
-                                    {address?.substring(0, 8)}...
-                                </p>
-                                <p className="text-sm text-muted-foreground">1 ETH</p>
+            <DrawerContent className="rounded-tl-2xl rounded-bl-2xl w-[100px] space-y-2 flex flex-col justify-between h-full">
+                <div>
+                    <DrawerHeader>
+                        <DrawerTitle>Account</DrawerTitle>
+                    </DrawerHeader>
+                    <div className="flex flex-col gap-4 shrink-0">
+                        <div className="flex items-center justify-between px-4">
+                            <div className="inline-flex items-center gap-4">
+                                <AvatarImage string={address} size={40} />
+                                <div className="flex flex-col justify-center">
+                                    <p className="font-semibold text-foreground">
+                                        {address?.substring(0, 8)}...
+                                    </p>
+                                    <p className="text-sm text-muted-foreground">1 ETH</p>
+                                </div>
                             </div>
+                            <Badge>Connected</Badge>
                         </div>
-                        <Badge>Connected</Badge>
                     </div>
-                    <div className="flex flex-col gap-2 px-4">
-                        <AlertDialog>
-                            <AlertDialogTrigger asChild>
-                                <Button variant={'outline'} className="w-full">
-                                    Disconnect
-                                </Button>
-                            </AlertDialogTrigger>
-                            <AlertDialogContent className="w-[400px]">
-                                <AlertDialogHeader>
-                                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                                </AlertDialogHeader>
-                                <AlertDialogFooter>
-                                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                                    <AlertDialogAction
-                                        variant={'default'}
-                                        onClick={() => disconnect()}
-                                    >
-                                        Yes, sure!
-                                    </AlertDialogAction>
-                                </AlertDialogFooter>
-                            </AlertDialogContent>
-                        </AlertDialog>
-                    </div>
+                </div>
+                <div className="px-4 pb-4">
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button variant={'default'} className="w-full">
+                                Disconnect
+                            </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent className="w-[400px]">
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction
+                                    variant={'default'}
+                                    onClick={() => disconnect()}
+                                >
+                                    Yes, sure!
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
                 </div>
             </DrawerContent>
         </Drawer>
