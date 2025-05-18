91313a50806a554460667164d0bc3f698f7cb802
billalxcode <billal.xcode@gmail.com>
2025-05-18 20:31:35 +0700

    fix: add 'latin' subset to Work_Sans font configuration for improved text rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import AppProvider from '@/context';
 
 const workSans = Work_Sans({
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
