e376433bdd4e26427a541c9a23758f27e42cd419
billalxcode <billal.xcode@gmail.com>
2025-05-26 07:55:39 +0700

    fix: add async rewrites configuration for API routing

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
 		domains: ['dummyimage.com', 'source.boringavatars.com', 'placehold.co'],
 	},
 	/* config options here */
+	async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:8000/api/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
