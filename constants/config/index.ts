b9bbeff90f585ce2b849f2d7d4abac5c96657303
billalxcode <billal.xcode@gmail.com>
2025-05-26 08:33:29 +0700

    fix: remove default projectId value to ensure explicit configuration

--- constants/config/index.ts
+++ constants/config/index.ts
@@ -4,7 +4,7 @@ import { localhost } from '@reown/appkit/networks';
 import type { AppKitNetwork } from '@reown/appkit/networks';
 
 // Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID || 'b56e18d47c72ab683b10814fe9495694'; // this is a public projectId only to use on localhost
+export const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID; // this is a public projectId only to use on localhost
 
 if (!projectId) {
 	throw new Error('Project ID is not defined');
