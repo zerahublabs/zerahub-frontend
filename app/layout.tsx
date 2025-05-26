617600b9f4bf75293339d6d82e0cff62f78fa0a6
billalxcode <billal.xcode@gmail.com>
2025-05-26 07:55:56 +0700

    fix: remove duplicate import of globals.css in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import './globals.css';
 import type { Metadata } from 'next';
 import { Work_Sans } from 'next/font/google';
-import './globals.css';
 import WalletContextProvider from '@/context/wallet';
 import { headers } from 'next/headers';
 import AppProvider from '@/context';
