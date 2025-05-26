c7c9af1898534f85a9b568d88eb06e14dc465dd1
billalxcode <billal.xcode@gmail.com>
2025-05-26 08:33:36 +0700

    feat: enhance AppProvider with loading state and alert dialog for wallet login

--- context/index.tsx
+++ context/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, Suspense, useMemo, useState } from 'react';
 import {
 	Sidebar,
 	SidebarActionNewDataset,
@@ -9,6 +9,19 @@ import {
 import { IconBrandTabler, IconDatabase, IconSettings } from '@tabler/icons-react';
 import { cn } from '@/lib/utils';
 import { Header } from '@/components/ui/header';
+import Loading from '@/components/ui/loading';
+import { useAuth } from '@/hooks/use-auth';
+import {
+	AlertDialog,
+	AlertDialogContent,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogAction,
+	AlertDialogDescription,
+} from '@/components/ui/alert-dialog';
+import { LoaderCircle } from 'lucide-react';
+import UpdateUsernameDialog from '@/components/block/dialogs/update-username';
 
 export const Logo = () => {
 	return (
@@ -33,35 +46,41 @@ export const LogoIcon = () => {
 };
 
 export default function AppProvider(props: { children: ReactNode }) {
-	const links = [
-		{
-			label: 'Discover',
-			href: '/',
-			icon: <IconBrandTabler className="h-5 w-5 shrink-0 text-card-foreground" />,
-		},
-		{
-			label: 'My Datasets',
-			href: '/my-datasets',
-			icon: <IconDatabase className="h-5 w-5 shrink-0 text-card-foreground" />,
-		},
-		{
-			label: 'Settings',
-			href: '#',
-			icon: <IconSettings className="h-5 w-5 shrink-0 text-card-foreground" />,
-		},
-	];
-	const [open, setOpen] = useState(true);
+	const links = useMemo(
+		() => [
+			{
+				label: 'Discover',
+				href: '/',
+				icon: <IconBrandTabler className="h-5 w-5 shrink-0 text-card-foreground" />,
+			},
+			{
+				label: 'My Datasets',
+				href: '/my-datasets',
+				icon: <IconDatabase className="h-5 w-5 shrink-0 text-card-foreground" />,
+			},
+			{
+				label: 'Settings',
+				href: '#',
+				icon: <IconSettings className="h-5 w-5 shrink-0 text-card-foreground" />,
+			},
+		],
+		[],
+	);
+
+	const [isSidebarOpen, setSidebarOpen] = useState(true);
+	const { isShownSignMessage, loading, isNeedsUsername, setIsNeedsUsername, onSignMessage } =
+		useAuth();
 
 	return (
 		<div
 			className={cn(
-				'mx-auto h-screen flex w-full max-w-screen flex-1 flex-col overflow-hidden rounded-md border bg-background md:flex-row',
+				'mx-auto min-h-screen flex w-full max-w-screen flex-1 flex-col overflow-hidden rounded-md border bg-background md:flex-row',
 			)}
 		>
-			<Sidebar open={open} setOpen={setOpen}>
+			<Sidebar open={isSidebarOpen} setOpen={setSidebarOpen}>
 				<SidebarBody className="justify-between gap-10">
 					<div className="flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
-						{open ? <Logo /> : <LogoIcon />}
+						{isSidebarOpen ? <Logo /> : <LogoIcon />}
 						<div className="mt-8 flex flex-col gap-2">
 							{links.map((link, idx) => (
 								<SidebarLink key={idx} link={link} />
@@ -75,8 +94,27 @@ export default function AppProvider(props: { children: ReactNode }) {
 			</Sidebar>
 			<div className="flex flex-col w-full min-h-screen">
 				<Header />
+				<div className="h-full overflow-y-auto flex-1 min-h-0">
+					<Suspense fallback={<Loading />}>{props.children}</Suspense>
+				</div>
 
-				<div className="w-full h-full overflow-y-auto">{props.children}</div>
+				<AlertDialog open={isShownSignMessage}>
+					<AlertDialogContent className="w-sm">
+						<AlertDialogHeader>
+							<AlertDialogTitle>Login with wallet</AlertDialogTitle>
+							<AlertDialogDescription>
+								Click below to sign a message and login.
+							</AlertDialogDescription>
+						</AlertDialogHeader>
+						<AlertDialogFooter>
+							<AlertDialogAction disabled={loading} onClick={onSignMessage}>
+								{loading && <LoaderCircle className="animate-spin" />}
+								Sign Message
+							</AlertDialogAction>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialog>
+				<UpdateUsernameDialog isOpen={isNeedsUsername} setIsOpen={setIsNeedsUsername} />
 			</div>
 		</div>
 	);
