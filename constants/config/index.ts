a19eab4e1fde914a3eed2bb9220f2f3d84253c19
billalxcode <billal.xcode@gmail.com>
2025-05-28 19:13:24 +0700

    fix: ensure projectId is explicitly typed as string for better type safety

--- constants/config/index.ts
+++ constants/config/index.ts
@@ -4,7 +4,7 @@ import { localhost } from '@reown/appkit/networks';
 import type { AppKitNetwork } from '@reown/appkit/networks';
 
 // Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID; // this is a public projectId only to use on localhost
+export const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID as string; // this is a public projectId only to use on localhost
 
 if (!projectId) {
 	throw new Error('Project ID is not defined');
