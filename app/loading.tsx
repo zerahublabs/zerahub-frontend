512a8067b937cab269f44a5f6b78e8adfbc981ad
billalxcode <billal.xcode@gmail.com>
2025-05-31 16:30:15 +0700

    style(loading): adjust loading component dimensions to use full screen width and height for improved layout

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Loading() {
 	return (
-		<div className="flex items-center justify-center w-full h-full">
+		<div className="flex items-center justify-center w-screen h-screen">
 			<Loader />
 		</div>
 	);
