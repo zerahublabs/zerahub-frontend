1a16cfd88418cb53e06578b2f4cd701a611a286b
billalxcode <billal.xcode@gmail.com>
2025-06-06 08:32:34 +0700

    fix(config): update environment variable from NEXT_PUBLIC_PROJECT_ID to REOWN_PROJECT_ID for consistency with new configuration standards

--- constants/config/index.ts
+++ constants/config/index.ts
@@ -17,7 +17,7 @@ export const ZeraHubChain = /*#__PURE__*/ defineChain({
 });
 
 // Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string; // this is a public projectId only to use on localhost
+export const projectId = process.env.REOWN_PROJECT_ID as string; // this is a public projectId only to use on localhost
 
 if (!projectId) {
 	throw new Error('Project ID is not defined');
