1b73e5e77770b099509fa6eefc3b9438095109b0
billalxcode <billal.xcode@gmail.com>
2025-06-09 21:40:33 +0700

    refactor(explore): remove redundant columns description and streamline layout by integrating FileExplorer

--- components/pages/collection/explore.tsx
+++ components/pages/collection/explore.tsx
@@ -16,14 +16,6 @@ export function ExploreSkeleton() {
 		<>
 			<Card>
 				<CardHeader>
-					<CardTitle>Columns Description</CardTitle>
-				</CardHeader>
-				<CardContent>
-					<Skeleton className="w-full h-48" />
-				</CardContent>
-			</Card>
-			<Card>
-				<CardHeader>
 					<CardTitle>Dataset Files</CardTitle>
 				</CardHeader>
 				<CardContent>
@@ -46,70 +38,7 @@ export function ExploreSkeleton() {
 export default function Explore() {
 	return (
 		<>
-			<Card>
-				<CardHeader>
-					<CardTitle>Columns Description</CardTitle>
-				</CardHeader>
-				<CardContent>
-					<Table className="border">
-						<TableHeader>
-							<TableRow>
-								<TableHead>Column</TableHead>
-								<TableHead>Description</TableHead>
-							</TableRow>
-						</TableHeader>
-						<TableBody>
-							{[
-								{ name: 'Name', description: 'Name of the dataset' },
-								{ name: 'Size', description: 'Size of the dataset' },
-								{ name: 'Type', description: 'Type of the dataset' },
-								{ name: 'Price', description: 'Price of the dataset' },
-							].map((item) => (
-								<TableRow key={item.name}>
-									<TableCell>{item.name}</TableCell>
-									<TableCell>{item.description}</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-				</CardContent>
-			</Card>
-			<Card>
-				<CardHeader>
-					<CardTitle>Dataset Files</CardTitle>
-				</CardHeader>
-				<CardContent>
-					<Table className="border">
-						<TableHeader>
-							<TableRow>
-								<TableHead>File Name</TableHead>
-								<TableHead>Size</TableHead>
-								<TableHead>Type</TableHead>
-							</TableRow>
-						</TableHeader>
-						<TableBody>
-							{[
-								{
-									name: 'dataset.csv',
-									size: '20 MB',
-									type: 'CSV',
-								},
-								{
-									name: 'dataset.json',
-									size: '10 MB',
-									type: 'JSON',
-								},
-							].map((item) => (
-								<TableRow key={item.name}>
-									<TableCell>{item.name}</TableCell>
-									<TableCell>{item.size}</TableCell>
-									<TableCell>{item.type}</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-				</CardContent>
-			</Card>
+			<FileExplorer />
 			<Card>
 				<CardHeader>
 					<CardTitle>Samples</CardTitle>
@@ -142,7 +71,6 @@ export default function Explore() {
 					</Table>
 				</CardContent>
 			</Card>
-			<FileExplorer />
 		</>
 	);
 }
